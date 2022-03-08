e98469735a3873f5203170b14ccc5f502347ac69
yamachan32 <apropiado.para.m@gmail.com>
2022-03-08 14:44:16 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,59 @@
+class PrototypesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :edit, :destoroy]
+
+  def index
+    @prototypes = Prototype.all.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    prototype = Prototype.create(prototype_params)
+    if prototype.save
+      redirect_to root_path
+    else
+      @prototype = Prototype.new(prototype_params)
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    unless user_signed_in? && params[:id] == current_user.id
+      redirect_to action: :index
+    end
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to root_path
+    else
+      @prototype = prototype
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+end
